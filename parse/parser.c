f23ca59c4a293ac48f48a99834165dcfe6d5ee62
abderrahim <chikhi.dev@gmail.com>
2024-05-17 17:32:14 +0100

    push

--- parse/parser.c
+++ parse/parser.c
@@ -29,36 +29,62 @@ int is_white_space(char c)
     return (c == ' ' || (c >= 9 && c <= 13));
 }
 
-void    validate_cmd(char *cmd, t_root *root)
+char *concat_str(t_root *root, char *buffer, char _char_)
 {
-    int     i;
-    char    *buffer;
     char    *tmp;
     char    *current;
 
+    current = malloc(sizeof(char) * 2);
+    if (!current)
+        error("malloc failed", root);
+    current[0] = _char_;
+    current[1] = '\0';
+    tmp = ft_strjoin(buffer, current);
+    if (!tmp)
+        error("malloc failed", root);
+    free(current);
+    free(buffer);
+    return tmp;
+}
+
+void    extract_cmd(char *cmd, t_root *root)
+{
+    int     i;
+    int     j;
+    char    *buffer;
+    int     quoted;
+    char    quote;
+    char    *tmp;
+
     buffer = NULL;
     i = 0;
+    quoted = 0;
+    while (is_white_space(cmd[i]))
+        i++;
     while (cmd[i])
     {
-        while (is_white_space(cmd[i]) && cmd[i])
-            i++;
-        if (!cmd[i])
-            break ;
-        if (cmd[i] != '\"' && cmd[i] != '\'')
+        if (cmd[i] == '\'' || cmd[i] == '\"')
         {
-            current = malloc(sizeof(char) * 2);
-            if (!current)
-                error("malloc failed", root);
-            current[0] = cmd[i];
-            current[1] = '\0';
-            tmp = ft_strjoin(buffer, current);
-            if (!tmp)
-                error("malloc failed", root);
-            free(buffer);
-            free(current);
-            buffer = tmp;
+            quoted = !quoted;
+            quote = cmd[i];
         }
+        if (is_white_space(cmd[i]) && !quoted)
+            break ;
+        buffer = concat_str(root, buffer, cmd[i]);
         i++;
+    }
+    while (quoted)
+    {
+        printf("\n");
+        j = 0;
+        tmp = readline("dqoute> ");
+        while (tmp[j])
+        {
+            if ((tmp[j] == '\"' || tmp[j] == '\'') && tmp[j] == quote)
+                quoted = 0;
+            buffer = concat_str(root, buffer, tmp[j]);
+            j++;
+        }
     }
     root->commands[root->num_commands]->cmd = buffer;
     printf("(off) cmd: %s\n", root->commands[root->num_commands]->cmd);
@@ -75,7 +101,7 @@ void    generate_cmds(char **cmds, t_root *root)
         root->commands[i] = malloc(sizeof(t_command));
         if (!root->commands[i])
             error("malloc failed", root);
-        validate_cmd(cmds[i], root);
+        extract_cmd(cmds[i], root);
         root->num_commands ++; 
     }
 }
