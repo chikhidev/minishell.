bd3d719d2d458d5cdd20c1a581040311f29af33a
chikhidev <chikhi.dev@gmail.com>
2024-08-25 21:41:28 +0100

    pusj

--- main.c
+++ main.c
@@ -127,6 +127,62 @@ void free_environment(t_db  *db)
     }
 }
 
+
+void print_nodes(t_db   *db, void *node, int level)
+{
+    if (!node) return ;
+
+    for (int i = 0; i < level; i++) 
+        printf("  ");
+    if (level > 0) printf("↳");
+    if (CMD->type == CMD_NODE)
+    {
+        if (ft_strcmp(CMD->args[0], "echo") == 0)
+            echo(db, CMD->args, 3);
+        else if (ft_strcmp(CMD->args[0], "export") == 0)
+            export(db, CMD->args);
+        else if (ft_strcmp(CMD->args[0], "pwd") == 0)
+            pwd(db);
+        else if (ft_strcmp(CMD->args[0], "env") == 0)
+            env(db, CMD->args);
+        else if (ft_strcmp(CMD->args[0], "cd") == 0)
+            cd(db, CMD->args);
+        else if (ft_strcmp(CMD->args[0], "unset") == 0)
+            unset(db, CMD->args);
+        else if (ft_strcmp(CMD->args[0], "exit") == 0)
+        {
+            error(db, NULL, NULL);
+            ec_void(db);
+            exit(0);
+        }
+        else
+        {
+            printf("CMD->> ");
+            for (int i = 0; CMD->args[i]; i++) {
+                printf("[%s] ", CMD->args[i]);
+            }
+            if (CMD->input_fd != STDIN_FILENO) printf("IN: %d ", CMD->input_fd);
+            if (CMD->output_fd != STDOUT_FILENO) printf("OUT: %d", CMD->output_fd);
+            printf("\n");
+        }
+        return ;
+    }
+    else if (OP->type == OP_NODE)
+    {
+        printf(MAGENTA);
+        if (OP->op_presentation == AND) printf("OP->> AND\n");
+        else if (OP->op_presentation == OR) printf("OP->> OR\n");
+        else if (OP->op_presentation == PIPE) printf("OP->> PIPE\n");
+        printf(RESET);
+    }
+    
+    for (int i = 0; i < OP->n_childs; i++)
+    {
+        print_nodes(db, OP->childs[i], level + 1);
+    }
+}
+
+
 int     main(int    ac, char    *av[],  char    *env[])
 {
     t_db    db;
@@ -153,8 +209,11 @@ int     main(int    ac, char    *av[],  char    *env[])
         line = tmp;
         if (parser(&db, line) == FAILURE)
             continue ;
-        if (exec(&db, db.root_node) == FAILURE)
-            continue ;
+
+        print_nodes(&db, db.root_node, 0);
+
+        // if (exec(&db, db.root_node) == FAILURE)
+        //     continue ;
         gc_void(&db);
     }
     free_environment(&db);
