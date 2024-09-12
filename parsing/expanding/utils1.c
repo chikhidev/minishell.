a965024429cf78dd256e249bc6f613f9e172f610
chikhidev <chikhi.dev@gmail.com>
2024-09-12 06:15:27 +0100

    norm

--- parsing/expanding/utils1.c
+++ parsing/expanding/utils1.c
@@ -6,7 +6,7 @@
 /*   By: abchikhi <abchikhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 02:48:03 by abchikhi          #+#    #+#             */
-/*   Updated: 2024/09/12 02:48:04 by abchikhi         ###   ########.fr       */
+/*   Updated: 2024/09/12 06:13:22 by abchikhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ char	*get_environment_var(t_db *db, char *var, char *env[])
 	while (env[i])
 	{
 		if (ft_strncmp(temp, env[i], ft_strlen(temp)) == 0)
-		{
 			return (env[i] + ft_strlen(var) + 1);
-		}
 		i++;
 	}
 	return (NULL);
@@ -49,63 +47,42 @@ char	*get_env(t_db *db, char *name)
 	return (ft_strdup(db, ""));
 }
 
-// int	valid_char(char c, int index)
-// {
-// 	// (void)index;
-// 	if ((isdigit(c) && index > 1))
-// 		return (false);
-
-// 	if (ft_isalnum(c))
-// 		return (true);
-// 	if (c == '?' || c == '_')
-// 		return (true);
-// 	return (false);
-// }
+bool	valid(char **line, int *i, bool *is_digit, bool *is_special_char)
+{
+	if (!ft_isalnum((*line)[(*i)]) && (*line)[(*i)] != '?'
+		&& (*line)[(*i)] != '_')
+		return (false);
+	if (*is_special_char)
+		return (false);
+	if (ft_isdigit((*line)[(*i)]))
+		*is_digit = true;
+	if ((*line)[(*i)] == '?' || (*line)[(*i)] == '_')
+		*is_special_char = true;
+	if (ft_isalpha((*line)[(*i)]) && *is_digit)
+		return (false);
+	return (true);
+}
 
 int	concat_env_name(char **line, char **env_var_name, int *i, t_quote *q)
 {
 	char	*tmp;
 	t_db	*db;
-	bool 	is_digit;
-	bool 	is_special_char;
-
+	bool	is_digit;
+	bool	is_special_char;
 
 	db = this();
 	tmp = NULL;
-
 	is_digit = false;
 	is_special_char = false;
-
 	while ((*line)[(*i)])
 	{
 		if (q && is_quote(q, *i))
-		{
 			return (SUCCESS);
-		}
-		if (!ft_isalnum((*line)[(*i)]) && (*line)[(*i)] != '?' && (*line)[(*i)] != '_')
-		{
-			return (FAILURE);
-		}
-		if (is_special_char)
-		{
-			return FAILURE;
-		}
-		if (ft_isdigit((*line)[(*i)]))
-			is_digit = true;
-		if ((*line)[(*i)] == '?' || (*line)[(*i)] == '_')
-			is_special_char = true;
-		if (ft_isalpha((*line)[(*i)]))
-		{
-			if (is_digit)
-			{
-				return FAILURE;
-			}
-		}
-
+		if (!valid(line, i, &is_digit, &is_special_char))
+			break ;
 		tmp = concat(db, *env_var_name, (*line)[(*i)]);
 		*env_var_name = tmp;
 		(*i)++;
 	}
-	// printf("len: %d\n", len);
 	return (SUCCESS);
 }
